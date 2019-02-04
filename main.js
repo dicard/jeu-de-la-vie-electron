5f38b966108066b3c332884ed94f33ded6c98795
Dicard <robin.corona31@gmail.com>
2019-02-04 11:38:20 +0100

    first commit, mise en place eslint/mocha

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+const createWindow = () => {
+
+  mainWindow = new BrowserWindow({width: 1800, height: 1200}); // on définit une taille pour notre fenêtre
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`); // on doit charger un chemin absolu
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+
+app.on('ready', createWindow);
