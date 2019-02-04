ee472b497ac3ee711ae9d3dc9fbf401b041e3747
Dicard <robin.corona31@gmail.com>
2019-02-04 20:02:54 +0100

    generation d'une nouvelle partie aléatoire a partir d'une taille predefinie(max 80*40)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-
+const { ipcMain } = require('electron');
 const app = electron.app;
 
 const BrowserWindow = electron.BrowserWindow;
@@ -8,14 +8,36 @@ let mainWindow;
 
 const createWindow = () => {
 
-  mainWindow = new BrowserWindow({width: 1800, height: 1200}); // on définit une taille pour notre fenêtre
+  mainWindow = new BrowserWindow({width: 1800, height: 1200});
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`); // on doit charger un chemin absolu
+  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
-
 app.on('ready', createWindow);
+
+//methods
+exports.generateRandomStart = (gridData) => {
+  var randomArray= new Array(gridData.height);
+
+    for(var i = 0; i < gridData.height; i++) {
+      randomArray[i]= [];
+      for(var j = 0; j < gridData.width; j++) {
+        randomArray[i][j] = Math.floor(Math.random() * 2);
+      }
+    }
+
+    return randomArray;
+    
+}
+
+
+//ipc Things
+
+ipcMain.on('new-game',(event, gridData) => {
+  var randomArray = this.generateRandomStart(gridData);
+  event.sender.send('randomArray', randomArray);
+})
---
+++ renderer/renderer.js
@@ -0,0 +1,71 @@
+const $ = require('jquery');
+const { ipcRenderer } = require('electron');
+
+// var app = {
+//   gridData: {
+//     width:80,
+//     height: 40,
+//     configuration:"1"
+//   },
+
+//   init: () => {
+  
+//     $('#configuration').on('click',ipcRenderer.send('new-game', $()));
+//   },
+
+//   setGridData: () => {
+//     console.log($('#grid-width').val());
+    
+
+//   },
+
+//   generateGrid: () => {
+//     app.grid = $('#grid');
+  
+//     for(var i = 0; i < app.gridData.height; i++){      
+//       for(var j = 0; j < app.gridData.width; j++){
+//         app.grid.append('<div class="cell dead-cell"></div>')
+//       }
+//     }
+//   }
+  
+
+// };
+
+var newGame = (event) => {
+  event.preventDefault();
+  var width = ($('#grid-width').val() > 80) ? 80 : $('#grid-width').val();
+
+  var height= ($('#grid-height').val()) > 40 ? 40 : $('#grid-height').val();
+  
+  this.gridData= {
+    cssWidth: (width*20) + (width*0.4),
+    width: width,
+    height: height,
+  };
+  ipcRenderer.send('new-game', this.gridData)
+}
+
+ipcRenderer.on('randomArray', (event, randomArray) => {  
+  var className = '';
+  var grid = $('#grid');
+
+  grid.empty();
+  grid.css('width', this.gridData.cssWidth);
+  
+  
+  for(var i = 0; i < this.gridData.height; i++){
+    for(var j = 0; j < this.gridData.width; j++) {
+      
+      if(randomArray[i][j] === 1){
+        className = 'cell alive-cell';
+      }
+      else {
+        className ='cell dead-cell';
+      }
+      grid.append($(document.createElement('div')).addClass(className));
+    }
+  }
+
+})
+  
